8927c96002c982be7de0e08c4ead2c2a75901435
rkddngpr <rkddngpr@unist.ac.kr>
2025-08-18 18:53:23 +0900

    api url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import {
   AlertCircle,
 } from "lucide-react";
 import "./App.css"; // CSS 파일 import
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "https://bokjikok.onrender.com";
 
 
 const WelfareApp = () => {
